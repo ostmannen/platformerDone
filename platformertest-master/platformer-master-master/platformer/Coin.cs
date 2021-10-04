cfb22c1d79e96188f524768a6b7a93962c0d5625
ostmannen <60339614+ostmannen@users.noreply.github.com>
2021-10-04 12:27:42 +0200

    first commit

---
+++ platformertest-master/platformer-master-master/platformer/Coin.cs
@@ -0,0 +1,25 @@
+using System;
+using SFML.System;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace platformer
+{
+    public class Coin : Entity
+    {
+        public static int coins;
+         public Coin() : base("tileset")
+        {
+            sprite.TextureRect = new IntRect(200, 127, 16, 16);
+            sprite.Origin = new Vector2f(8, 8);          
+        } 
+        public override void Update(Scene scene, float deltaTime){
+            if (scene.FindByType<Hero>(out Hero hero)){
+                if(Collision.RectangleRectangle(Bounds, hero.Bounds, out _)){
+                    scene.coins++;
+                    Dead = true;
+                }
+            }
+        }
+    }
+}
